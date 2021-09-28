8398d66f2d817635d8adf6f4d67c1bec5c3c5f1f
Musa1406 <91537282+Musa1406@users.noreply.github.com>
2021-09-28 14:00:26 +0300

    Initial commit

---
+++ src/ogrenciBilgileri/Main.java
@@ -0,0 +1,17 @@
+package ogrenciBilgileri;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// sysout ctrl space
+		System.out.println("Merhaba Musa");
+		System.out.print("Musa");
+		System.out.print(" ");
+		System.out.println("Acili");
+		System.out.println("Musa" + " " + "Acili");
+		System.out.println("Yaþ :"+15);
+		System.out.println("Boy :"+178);
+	}
+
+}
